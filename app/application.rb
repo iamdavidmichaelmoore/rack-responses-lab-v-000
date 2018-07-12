beb612dff603ece83ce6c03026c4d27d9138e7c5
David Moore <hey@iamdavidmoore.com>
2018-07-12 15:35:26 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,8 +1,11 @@
+require 'pry'
+
 class Application
 
   def call(env)
     resp = Rack::Response.new
     current_time = Time.new
+    binding.pry
     if current_time.hour < 12
       resp.write "Good Morning!"
     elsif current_time.hour > 12
